2394d4f92205f2f921d123e0b9d840e824cc55b5
Sharif Salah <sharifsalah@users.noreply.github.com>
2016-05-10 11:09:41 +0100

    Create swarm-setup-d8.sh
    Added a variant of the swarm setup script updated to support installation of Docker on Debian 8.

---
+++ swarm-setup-d8.sh
@@ -0,0 +1,112 @@
+#! /bin/sh
+#
+# File: swarm-setup.sh
+#
+# Purpose: Complete setup steps for the Jenkins worker node instance
+#
+# Pre-conditions:
+#  Debian 8 OS
+#  slave.jar is downloaded to the user home directory
+#  This script is run from the Git repo directory
+#
+
+echo 'Changing to user home directory'
+cd
+
+echo 'Installing Docker...'
+sudo apt-get purge lxc-docker*
+sudo apt-get purge docker.io*
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo bash -c "echo deb https://apt.dockerproject.org/repo debian-jessie main >> /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-cache policy docker-engine
+sudo apt-get install docker-engine
+sudo service docker start
+sudo docker run hello-world
+sudo gpasswd -a $USER docker
+sudo service docker restart
+
+sudo addgroup build
+sudo adduser --disabled-password --system --ingroup build jenkins
+sudo mkdir /home/jenkins/build
+sudo chown jenkins:build /home/jenkins/build
+
+# Install the Jenkins build agent agent code
+
+echo 'Installing Jenkins Slave software'
+sudo mkdir -p /opt/jenkins-slave
+sudo mv slave.jar /opt/jenkins-slave
+sudo chown -R root:root /opt/jenkins-slave
+
+echo 'Installing Jenkins Swarm plugin client-side software'
+wget -q -O swarm-client-1.24.jar \
+http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/1.24/swarm-client-1.24-jar-with-dependencies.jar
+sudo mkdir -p /opt/swarm-client
+sudo mv swarm-client*.jar /opt/swarm-client/
+sudo chown -R root:root /opt/swarm-client
+
+echo 'Installing Swarm init.d script'
+cd
+tee swarm << 'EOF' > /dev/null
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          swarm
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: swarm
+# Description:       swarm daemon
+### END INIT INFO
+
+PATH=/sbin:/usr/sbin:/bin:/usr/bin:/opt/packer
+JPW=
+
+do_start () {
+        # start Swarm
+        exec java  -Xmx256m -Xmx256m -Dfile.encoding=UTF-8   -jar /opt/swarm-client/swarm-client-1.24.jar  -master http://jenkins-master:8080 -username admin -password $JPW -fsroot /home/jenkins -description 'auto' -labels 'slave' -name 'slave-auto' -executors 1 -mode exclusive
+}
+
+# stop case omitted as the instances are ephemeral
+case "$1" in
+  start|"")
+        do_start
+        ;;
+  *)
+        echo "Usage: swarm [start]" >&2
+        exit 3
+        ;;
+esac
+:
+EOF
+
+sudo chown root:root swarm
+sudo mv swarm /etc/init.d/
+sudo chmod 755 /etc/init.d/swarm
+sudo update-rc.d swarm defaults
+
+sudo sed -i "s|JPW=|JPW=$JENKINS_PW|g" /etc/init.d/swarm
+
+echo 'Installing UNZIP program'
+cd
+sudo apt-get -y -qq install unzip
+
+echo 'Installing Packer program'
+wget -q -O packer.zip https://releases.hashicorp.com/packer/0.9.0/packer_0.9.0_linux_amd64.zip
+sudo mkdir -p /opt/packer
+sudo unzip -d /opt/packer packer.zip
+sudo chown root:staff /opt/packer/*
+
+# Remove the Packer installation ZIP file
+rm packer.zip
+
+# Update gcloud SDK components
+sudo gcloud components update -q
+
+# Configure the Swarm service to start when the instance boots
+sudo update-rc.d swarm defaults
+
+
+echo 'Finished with installation script'

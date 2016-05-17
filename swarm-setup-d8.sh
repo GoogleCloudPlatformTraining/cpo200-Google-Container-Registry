afb629a44f06beb23c98c2d18b4f2aabc833b6fe
Sharif Salah <sharifsalah@users.noreply.github.com>
2016-05-17 15:41:09 +0100

    Update swarm-setup-d8.sh
    Commented out packer installation and Cloud SDK update

--- swarm-setup-d8.sh
+++ swarm-setup-d8.sh
@@ -76,16 +76,16 @@ cd
 sudo apt-get -y -qq install unzip
 
 echo 'Installing Packer program'
-wget -q -O packer.zip https://releases.hashicorp.com/packer/0.9.0/packer_0.9.0_linux_amd64.zip
-sudo mkdir -p /opt/packer
-sudo unzip -d /opt/packer packer.zip
-sudo chown root:staff /opt/packer/*
+# wget -q -O packer.zip https://releases.hashicorp.com/packer/0.9.0/packer_0.9.0_linux_amd64.zip
+# sudo mkdir -p /opt/packer
+# sudo unzip -d /opt/packer packer.zip
+# sudo chown root:staff /opt/packer/*
 
 # Remove the Packer installation ZIP file
-rm packer.zip
+# rm packer.zip
 
 # Update gcloud SDK components
-sudo gcloud components update -q
+# sudo gcloud components update -q
 
 # Configure the Swarm service to start when the instance boots
 sudo supervisorctl reread
